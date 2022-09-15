c9d55342f5306e65083a0acb3f003d2be018a444
henishpatel9045 <200120107182@git.org.in>
2022-09-15 21:34:01 +0530

    Custom auth app created.

--- custom_auth/admin.py
+++ custom_auth/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+@admin.register(models.Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ['id']
+    
+@admin.register(models.Librarian)
+class LibrarianAdmin(admin.ModelAdmin):
+    list_display = ['id']
+    
