b1f6cc520e44169750101e43a332c83b1e3aca79
henishpatel9045 <200120107182@git.org.in>
2022-09-16 15:08:04 +0530

    Endpoint for signup added.

--- custom_auth/models.py
+++ custom_auth/models.py
@@ -28,6 +28,7 @@ class Librarian(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=20)
     address = models.TextField()
+    pin_code = models.CharField(max_length=10)
     is_new = models.BooleanField(default=True)
     
     def full_name(self):
