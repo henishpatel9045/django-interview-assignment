7b648c1c818fc24ba31064219fd886a8340f8e07
henishpatel9045 <200120107182@git.org.in>
2022-09-17 13:30:41 +0530

    Secured.

--- library_management/settings/db_select.py
+++ library_management/settings/db_select.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 
-USE_POSTGRES = True
+USE_POSTGRES = False
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 print(BASE_DIR)
---
+++ library_management/settings/prod.py
@@ -0,0 +1,4 @@
+from library_management.settings.common import *
+import os
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
